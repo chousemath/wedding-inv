13fbf764ba0ac6ccbdf523f1ac6f337e99b0d33d
Joseph <chousemath@gmail.com>
2020-07-12 09:43:18 +0900

    Update

--- src/scripts.js
+++ src/scripts.js
@@ -1,21 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    new Splide('.splide', {
-        type: 'loop',
-        pagination: false,
-        height: '100vh',
-    }).mount();
-
-    const c = document.createElement('canvas');
-    const ctx = c.getContext('2d');
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    c.width = w;
-    c.height = h;
-    document.body.appendChild(c);
-    setTimeout(() => {
-        //window.scrollTo(0, document.body.scrollHeight);
-        window.scrollTo(0, 1);
-        //document.body.style.overflow = 'hidden';
-        alert('done');
-    }, 1000);
-});
+//document.addEventListener('DOMContentLoaded', function () {
+//    new Splide('.splide', {
+//        type: 'loop',
+//        pagination: false,
+//        height: '100vh',
+//    }).mount();
+//
+//    const c = document.createElement('canvas');
+//    const ctx = c.getContext('2d');
+//    const w = window.innerWidth;
+//    const h = window.innerHeight;
+//    c.width = w;
+//    c.height = h;
+//    document.body.appendChild(c);
+//});
+setTimeout(() => {
+    //window.scrollTo(0, document.body.scrollHeight);
+    window.scrollTo(0, 1);
+    //document.body.style.overflow = 'hidden';
+}, 1);
