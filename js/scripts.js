30a190dee96b0b2be330930e79de918ebad1d838
jochoi0707 <chousemath@gmail.com>
2020-07-26 11:38:22 +0900

    Start to add sparkle animations

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 "use strict";
+
+var coverCanvas = document.getElementById('cover-canvas');
+var ctxCover = coverCanvas.getContext('2d');
+ctxCover.beginPath();
+ctxCover.arc(20, 1, 10, 0, 2 * Math.PI);
+ctxCover.stroke();
+ctxCover.fillStyle = '#fffaf3';
+ctxCover.fill();
---
+++ src/scripts.js
@@ -0,0 +1,11 @@
+const coverCanvas = document.getElementById('cover-canvas');
+const ctxCover = coverCanvas.getContext('2d');
+
+ctxCover.beginPath();
+// context.arc(x,y,r,sAngle,eAngle,counterclockwise);
+ctxCover.arc(2,1,1,0,2*Math.PI);
+ctxCover.stroke();
+ctxCover.fillStyle = '#fffaf3';
+ctxCover.fill();
+
+
