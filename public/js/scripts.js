84f20dd47b7f034758995ed16aa9ded0fdf9ab47
Joseph <chousemath@gmail.com>
2020-08-14 09:38:53 +0900

    Update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,9 +6,47 @@ function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o =
 
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
+function initMap() {
+  var apelgamo = {
+    lat: 37.574825,
+    lng: 126.979138
+  };
+  var mapGoogle = new google.maps.Map(document.getElementById('map-google'), {
+    center: apelgamo,
+    zoom: 16
+  });
+  var markerGoogle = new google.maps.Marker({
+    position: apelgamo,
+    map: mapGoogle
+  });
+}
+
 var handleResize = function handleResize() {
-  console.log('go');
-  dark.style.display = getOrientation() === 'portrait' ? 'none' : 'flex';
+  var isPortrait = getOrientation() === 'portrait';
+  dark.style.display = isPortrait ? 'none' : 'flex'; //setTimeout(() => {
+  //    console.log(window.innerWidth, window.innerHeight);
+  //    const cont = document.getElementById('detailed-image-container');
+  //    cont.style.width = `${window.innerWidth}px`;
+  //    cont.style.height = `${window.innerHeight}px`;
+  //    for(let x of cont.getElementsByClassName('slick-list')) {
+  //        cont.style.height = `${window.innerHeight}px`;
+  //    }
+  //    for(let x of cont.getElementsByClassName('slick-track')) {
+  //        cont.style.height = `${window.innerHeight}px`;
+  //    }
+  //    for (let x of document.getElementsByClassName('detail-image')) {
+  //        if (isPortrait) {
+  //            //x.style.maxHeight = null;
+  //            x.style.width = `${window.innerWidth}px`;
+  //            x.style.height = null;
+  //        } else {
+  //            //x.style.maxHeight = '100vw';
+  //            x.style.height = `${window.innerHeight*0.5}px`;
+  //            x.style.width = null
+  //            //x.style.width = 'auto';
+  //        }
+  //    }
+  //}, 100);
 };
 
 var copyAddress = function copyAddress() {
@@ -16,6 +54,7 @@ var copyAddress = function copyAddress() {
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   document.execCommand('copy');
+  showCopyMessage();
 };
 
 var getOrientation = function getOrientation() {
@@ -27,13 +66,87 @@ window.onresize = function () {
 };
 
 handleResize();
+
+var mapShow = function mapShow(id) {
+  document.getElementById(id).style.zIndex = 15;
+
+  var _iterator = _createForOfIteratorHelper(document.getElementsByClassName('section-map-box-internal')),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var x = _step.value;
+      if (x.id !== id) x.style.zIndex = 1;
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+};
+
+var canHideWarning = false;
+var imWarning = document.getElementById('im-warning');
+
+var imWarningLoaded = function imWarningLoaded() {
+  var x = document.getElementById('dark2');
+  x.style.opacity = 0;
+  x.style.display = 'flex';
+  $('#dark2').animate({
+    opacity: 1
+  }, 800);
+  setTimeout(function () {
+    return canHideWarning = true;
+  }, 800);
+};
+
+if (imWarning.complete) {
+  imWarningLoaded();
+} else {
+  imWarning.addEventListener('load', imWarningLoaded);
+  imWarning.addEventListener('error', function () {
+    console.log('error loading warning image');
+  });
+}
+
+var hideWarning = function hideWarning() {
+  if (!canHideWarning) return;
+  $('#dark2').animate({
+    opacity: 0
+  }, 500);
+  setTimeout(function () {
+    return document.getElementById('dark2').style.display = 'none';
+  }, 500);
+};
+
+var showGallery = function showGallery() {};
+
+var showCopyMessage = function showCopyMessage() {
+  var x = document.getElementById('dark3');
+  x.style.opacity = 0;
+  x.style.display = 'flex';
+  $('#dark3').animate({
+    opacity: 1
+  }, 300);
+  setTimeout(function () {
+    $('#dark3').animate({
+      opacity: 0
+    }, 1000);
+    setTimeout(function () {
+      return document.getElementById('dark3').style.display = 'none';
+    }, 1000);
+  }, 800);
+};
+
+var hideGallery = function hideGallery() {};
+
 $(document).ready(function () {
   window.scrollTo(1, 0);
   $('.your-class').slick({
     infinite: true,
     dots: false,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 6000,
     centerMode: true,
     lazyLoad: 'ondemand',
     slidesToShow: 1,
@@ -43,19 +156,53 @@ $(document).ready(function () {
     prevArrow: $('.prev'),
     nextArrow: $('.next')
   });
-  var mapBox = document.getElementById('section-map-box').getBoundingClientRect();
+  $('#detailed-image-container').slick({
+    infinite: true,
+    dots: false,
+    autoplay: false,
+    centerMode: true,
+    lazyLoad: 'ondemand',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    variableHeight: true,
+    prevArrow: false,
+    nextArrow: false //adaptiveHeight: true
 
-  var _iterator = _createForOfIteratorHelper(document.getElementsByClassName('map')),
-      _step;
-
-  try {
-    for (_iterator.s(); !(_step = _iterator.n()).done;) {
-      var x = _step.value;
-      x.height = mapBox.height.toString();
-    }
-  } catch (err) {
-    _iterator.e(err);
-  } finally {
-    _iterator.f();
-  }
+  });
+  var sectionMapBox = document.getElementById('section-map-box');
+  var mapBox = sectionMapBox.getBoundingClientRect();
+  var mapContainerKakao = document.getElementById('map-kakao');
+  var options = {
+    center: new kakao.maps.LatLng(37.574825, 126.979138),
+    level: 3
+  };
+  var mapKakao = new kakao.maps.Map(mapContainerKakao, options);
+  mapContainerKakao.style.width = mapBox.width;
+  mapContainerKakao.style.height = mapBox.height;
+  var markerPosKakao = new kakao.maps.LatLng(37.574825, 126.979138);
+  var markerKakao = new kakao.maps.Marker({
+    position: markerPosKakao
+  });
+  markerKakao.setMap(mapKakao);
+  var naverMapOptions = {
+    center: new naver.maps.LatLng(37.574825, 126.979138),
+    zoom: 16
+  };
+  var mapNaver = new naver.maps.Map('map-naver', naverMapOptions);
+  var markerNaver = new naver.maps.Marker({
+    position: new naver.maps.LatLng(37.574825, 126.979138),
+    map: mapNaver
+  });
+  var mapTmap = new Tmapv2.Map('map-tmap', {
+    center: new Tmapv2.LatLng(37.574825, 126.979138),
+    // 지도 초기 좌표
+    width: "".concat(mapBox.width, "px"),
+    height: "".concat(mapBox.height, "px"),
+    zoom: 16
+  });
+  var marker = new Tmapv2.Marker({
+    position: new Tmapv2.LatLng(37.574825, 126.979138),
+    map: mapTmap
+  });
 });
--- public/src/scripts.js
+++ public/src/scripts.js
@@ -1,13 +1,44 @@
+function initMap() {
+    const apelgamo = {lat: 37.574825, lng: 126.979138};
+    const mapGoogle = new google.maps.Map(document.getElementById('map-google'), {center: apelgamo, zoom: 16});
+    const markerGoogle = new google.maps.Marker({position: apelgamo, map: mapGoogle});
+}
+
 const handleResize = () => {
-    console.log('go');
-    dark.style.display = getOrientation() === 'portrait' ? 'none' : 'flex';
+    const isPortrait = getOrientation() === 'portrait';
+    dark.style.display = isPortrait ? 'none' : 'flex';
+    //setTimeout(() => {
+    //    console.log(window.innerWidth, window.innerHeight);
+    //    const cont = document.getElementById('detailed-image-container');
+    //    cont.style.width = `${window.innerWidth}px`;
+    //    cont.style.height = `${window.innerHeight}px`;
+    //    for(let x of cont.getElementsByClassName('slick-list')) {
+    //        cont.style.height = `${window.innerHeight}px`;
+    //    }
+    //    for(let x of cont.getElementsByClassName('slick-track')) {
+    //        cont.style.height = `${window.innerHeight}px`;
+    //    }
+    //    for (let x of document.getElementsByClassName('detail-image')) {
+    //        if (isPortrait) {
+    //            //x.style.maxHeight = null;
+    //            x.style.width = `${window.innerWidth}px`;
+    //            x.style.height = null;
+    //        } else {
+    //            //x.style.maxHeight = '100vw';
+    //            x.style.height = `${window.innerHeight*0.5}px`;
+    //            x.style.width = null
+    //            //x.style.width = 'auto';
+    //        }
+    //    }
+    //}, 100);
 };
 
 const copyAddress = () => {
-  const copyText = document.getElementById('myInput');
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  document.execCommand('copy');
+    const copyText = document.getElementById('myInput');
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    showCopyMessage();
 }
 
 const getOrientation = () => window.innerWidth > window.innerHeight ? 'landscape' : 'portrait';
@@ -16,13 +47,56 @@ window.onresize = function(){ handleResize() }
 
 handleResize();
 
+const mapShow = (id) => {
+    document.getElementById(id).style.zIndex = 15;
+    for (let x of document.getElementsByClassName('section-map-box-internal')) {
+        if (x.id !== id) x.style.zIndex = 1;
+    }
+};
+
+let canHideWarning = false;
+const imWarning = document.getElementById('im-warning');
+const imWarningLoaded = () => {
+    const x = document.getElementById('dark2');
+    x.style.opacity = 0;
+    x.style.display = 'flex';
+    $('#dark2').animate({opacity: 1}, 800);
+    setTimeout(() => canHideWarning = true, 800);
+}
+if (imWarning.complete) {
+    imWarningLoaded();
+} else {
+    imWarning.addEventListener('load', imWarningLoaded)
+    imWarning.addEventListener('error', function() {
+        console.log('error loading warning image');
+    });
+}
+const hideWarning = () => {
+    if (!canHideWarning) return;
+    $('#dark2').animate({opacity: 0}, 500);
+    setTimeout(() => document.getElementById('dark2').style.display = 'none', 500);
+}
+const showGallery = () => {};
+const showCopyMessage = () => {
+    const x = document.getElementById('dark3');
+    x.style.opacity = 0;
+    x.style.display = 'flex';
+    $('#dark3').animate({opacity: 1}, 300);
+    setTimeout(() => {
+        $('#dark3').animate({opacity: 0}, 1000);
+        setTimeout(() => document.getElementById('dark3').style.display = 'none', 1000);
+    }, 800);
+}
+const hideGallery = () => {
+}
+
 $(document).ready(function () {
     window.scrollTo(1, 0);
     $('.your-class').slick({
         infinite: true,
         dots: false,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 6000,
         centerMode: true,
         lazyLoad: 'ondemand',
         slidesToShow: 1,
@@ -32,10 +106,50 @@ $(document).ready(function () {
         prevArrow: $('.prev'),
         nextArrow: $('.next')
     });
-    const mapBox = document.getElementById('section-map-box').getBoundingClientRect();
-    for (let x of document.getElementsByClassName('map')) {
-        x.height = mapBox.height.toString();
-    }
+    $('#detailed-image-container').slick({
+        infinite: true,
+        dots: false,
+        autoplay: false,
+        centerMode: true,
+        lazyLoad: 'ondemand',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        variableHeight: true,
+        prevArrow: false,
+        nextArrow: false
+        //adaptiveHeight: true
+    });
+
+    const sectionMapBox = document.getElementById('section-map-box');
+    const mapBox = sectionMapBox.getBoundingClientRect();
+    const mapContainerKakao = document.getElementById('map-kakao');
+    const options = {
+    	center: new kakao.maps.LatLng(37.574825, 126.979138),
+    	level: 3
+    };
+    const mapKakao = new kakao.maps.Map(mapContainerKakao, options);
+    mapContainerKakao.style.width = mapBox.width;
+    mapContainerKakao.style.height = mapBox.height;
+
+    const markerPosKakao  = new kakao.maps.LatLng(37.574825, 126.979138);
+    const markerKakao = new kakao.maps.Marker({position: markerPosKakao});
+    markerKakao.setMap(mapKakao);
+
+    const naverMapOptions = {center: new naver.maps.LatLng(37.574825, 126.979138), zoom: 16};
+    const mapNaver = new naver.maps.Map('map-naver', naverMapOptions);
+    const markerNaver = new naver.maps.Marker({
+        position: new naver.maps.LatLng(37.574825, 126.979138),
+        map: mapNaver
+    });
+
+    const mapTmap = new Tmapv2.Map('map-tmap', {
+        center: new Tmapv2.LatLng(37.574825, 126.979138), // 지도 초기 좌표
+        width: `${mapBox.width}px`,
+        height: `${mapBox.height}px`,
+        zoom: 16
+    });
+    const marker = new Tmapv2.Marker({position: new Tmapv2.LatLng(37.574825, 126.979138), map: mapTmap});
 });
 
 
